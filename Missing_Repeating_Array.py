4dc6f91c60d41b97d32512c0af3688d34b7615d4
ShreyashPardhi <shreyashpardhi1@gmail.com>
2023-11-17 16:52:01 +0530

    day6 program1

---
+++ Missing_Repeating_Array.py
@@ -0,0 +1,28 @@
+'''
+find missing and repeating number in an array.
+'''
+
+
+def find_missing_and_repeating(nums):
+    n = len(nums)
+    ms=None
+    rn=None
+    for i in range (n):
+        if i<n-1 and nums[i]==nums[i+1]:
+            rn=nums[i]
+            nums.remove(nums[i])
+            n-=1
+            
+    if(nums[0]!=1):
+        ms=1
+    else:
+        for i in range(len(nums)):
+            if nums[i]!=i+1:
+                ms=i+1
+    return ms,rn
+
+# Example usage
+sorted_array = [1,2,2,3,5]
+missing, repeating = find_missing_and_repeating(sorted_array)
+print("Missing Number:", missing)
+print("Repeating Number:", repeating
